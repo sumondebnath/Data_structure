967c45206e70f41e81b611d7d7224ca6b442b498
sumon <debnathsumon1708@gmail.com>
2023-06-06 11:41:56 +0600

    commit

---
+++ basic data/hello.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"kjdfk";
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Sumon Debnath\n");
+    printf("%d\n", 5+6+7);
+
+    return 0;
+}
+
